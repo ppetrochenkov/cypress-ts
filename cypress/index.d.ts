24bbafe4fc6f27c41fa2a6e1c611f21bab8f1bd7
ppetrochenkov <philip.petrochenkov@gmail.com>
2023-12-07 15:52:22 +0300

    Revert "initial cypress framework setup, added sample tests"
    This reverts commit 20a0b41a020575c31270eb4903dcd57f1c744de5.

--- cypress/index.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="cypress" />
-
-declare namespace Cypress {
-    interface Chainable<Subject> {
-        byDataQa<E extends Node = HTMLElement>(selector: string, options?: Partial<Loggable & Timeoutable>): Chainable<JQuery<E>>,
-        containsValue<E extends Node = HTMLElement>(value: string): Chainable<JQuery<E>>,
-    }
-}
--- cypress/integration/src/page-objects/base-page.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types='cypress-xpath' />
-import {MainMenuComponent} from "@page-objects/main-menu-component";
-
-export abstract class BasePage  {
-
-    public mainMenu: MainMenuComponent = new MainMenuComponent();
-
-
-}
